a7b921e8818de57c1f1423d3aaf478eefff47c4f
Amstrong <monachelloa@uninorte.edu.co>
2019-11-05 00:17:35 -0500

    Agregando los puntajes

--- phaser_configuracion/src/scenes/scene_play.js
+++ phaser_configuracion/src/scenes/scene_play.js
@@ -5,17 +5,15 @@ class Scene_play extends Phaser.Scene {
   }
 
   create() {
-    var ScoreLeft = this.add.text(100, 6, 0, {
+    this.ScoreLeft = this.add.text(100, 6, 0, {
       color: "#ffffff",
       fontSize: 40
     });
-    var ScoreRight = this.add.text(this.sys.game.config.width - 100, 6, 0, {
+    this.ScoreRight = this.add.text(this.sys.game.config.width - 100, 6, 0, {
       color: "#ffffff",
       fontSize: 40
     });
 
-    var counterLeft = 0;
-    var counterRight = 0;
     let center_width = this.sys.game.config.width / 2;
     let center_height = this.sys.game.config.height / 2;
     let screen = this.sys.game.config.width;
@@ -59,11 +57,17 @@ class Scene_play extends Phaser.Scene {
     this.cursor_S = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.S
     );
-    ScoreLeft.text = counterLeft;
-    ScoreRight.text = counterRight;
+    this.valor1 = 0;
+    this.valor2 = 0;
   }
+  MarcadorIzquierda() {
+    this.ScoreLeft.text = this.valor1 += 1;
+  }
+  MarcadorDerecha() {
+    this.ScoreRight.text = this.valor2 += 1;
+  }
+
   update() {
-    //Control de las palas
     //Pala derecha
     if (this.cursor.down.isDown) {
       this.derecha.body.setVelocityY(280);
@@ -81,14 +85,13 @@ class Scene_play extends Phaser.Scene {
       this.izquierda.body.setVelocity(0);
     }
     if (this.ball.x < 0) {
-      //counterLeft += 1;
+      this.MarcadorDerecha();
       this.ball.setPosition(
         this.sys.game.config.width / 2,
         this.sys.game.config.height / 2
       );
     } else if (this.ball.x > this.sys.game.config.width) {
-      // counterRight += 1;
-      this.ScoreRight.add.text();
+      this.MarcadorIzquierda();
       this.ball.setPosition(
         this.sys.game.config.width / 2,
         this.sys.game.config.height / 2
@@ -98,6 +101,7 @@ class Scene_play extends Phaser.Scene {
   chocaPala() {
     this.ball.setVelocityY(Phaser.Math.Between(-150, 150));
   }
+
   //Metodo activo
 }
 
